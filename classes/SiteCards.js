03fa4e086b11d52ff035336c02105c135013ff6f
Terence Hastings <terencehastings.707@gmail.com>
2024-07-26 19:32:51 -0700

    update files

--- classes/SiteCards.js
+++ classes/SiteCards.js
@@ -7,10 +7,18 @@ export class SiteCards {
   temp(){
     let template = ``
     for(let i=0; i<this.#sites.length; i++){
-      template += `
+      if(this.#sites[i]["initial"] == "dk"){
+        template += `
+        <label for="${this.#sites[i]["initial"]}">${this.#sites[i]["name"]}</label>
+        <input id="select-site-${this.#sites[i]["initial"]}" type="radio" name="site-select" value="${this.#sites[i]["initial"]}" checked>
+        `;
+      }else{
+        template += `
       <label for="${this.#sites[i]["initial"]}">${this.#sites[i]["name"]}</label>
       <input id="select-site-${this.#sites[i]["initial"]}" type="radio" name="site-select" value="${this.#sites[i]["initial"]}" >
       `;
+      }
+      
     }
     return template;
   }
